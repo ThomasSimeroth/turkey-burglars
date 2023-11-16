7876f615bc202b1c4f5bd42f583b25387ec27303
Thomas Simeroth <thomassimeroth@gmail.com>
2023-11-16 12:52:34 -0500

    Fix game map test

--- src/main/java/com/levelup/forestsandmonsters/GameMap.java
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -1,10 +1,15 @@
 package com.levelup.forestsandmonsters;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameMap {
     private int numPositions;
+    private List<List<Position>> positions;
 
     public GameMap(){
         this.numPositions=100;
+        this.positions = new ArrayList<>();
     }
 
     public GameMap(int size){
@@ -15,5 +20,7 @@ public class GameMap {
         return this.numPositions;
     }
 
-    public List<List<Position>>
+    public List<List<Position>> getPoisitions() {
+        return positions;
+    }
 }
